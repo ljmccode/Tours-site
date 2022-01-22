d843653186c8ca31c1996e8e635e0771a5731d97
Laura <lmcsay@gmail.com>
2022-01-21 18:53:03 -0600

    Add smooth scroll functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // set copyright date
-const date = document.getElementById('date').innerHTML = new Date().getFullYear();
+const date = (document.getElementById('date').innerHTML =
+  new Date().getFullYear());
 
 // toggle navbar
 const toggle = document.getElementById('nav-toggle');
@@ -7,4 +8,24 @@ const links = document.getElementById('nav-links');
 
 toggle.addEventListener('click', () => {
   links.classList.toggle('show-links');
-})
+});
+
+// smooth scroll
+const scrollLinks = document.querySelectorAll('.scoll-link');
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    links.classList.remove('show-links');
+
+    const id = e.target.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth'
+    });
+  });
+});
