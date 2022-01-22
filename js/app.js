e730d12e52f2d68a9e0d5f5fc2c7875d7c87a903
Laura <lmcsay@gmail.com>
2022-01-21 18:38:21 -0600

    Add nav toggle functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // set copyright date
 const date = document.getElementById('date').innerHTML = new Date().getFullYear();
+
+// toggle navbar
+const toggle = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
+
+toggle.addEventListener('click', () => {
+  links.classList.toggle('show-links');
+})
